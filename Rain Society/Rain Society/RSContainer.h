394d5ba0d33aa728996088ed68c24eb128dab27e
Jucelio Fonseca <juceliofonseca@MacBook-Pro-de-Jucelio.local>
2015-03-26 11:34:14 -0300

    Interface first commit

---
+++ Rain Society/Rain Society/RSContainer.h
@@ -0,0 +1,13 @@
+//
+//  RSContainer.h
+//  Rain Society
+//
+//  Created by Jucelio Fonseca on 26/03/15.
+//  Copyright (c) 2015 Jucelio Fonseca. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface RSContainer : SKSpriteNode
+
+@end
---
+++ Rain Society/Rain Society/RSContainer.m
@@ -0,0 +1,13 @@
+//
+//  RSContainer.m
+//  Rain Society
+//
+//  Created by Jucelio Fonseca on 26/03/15.
+//  Copyright (c) 2015 Jucelio Fonseca. All rights reserved.
+//
+
+#import "RSContainer.h"
+
+@implementation RSContainer
+
+@end
