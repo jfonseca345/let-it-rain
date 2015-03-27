b215c49bac251ec5c5a71d3fc2795c351b5db43b
Jucelio Fonseca <juceliofonseca@MacBook-Pro-de-Jucelio.local>
2015-03-27 10:54:27 -0300

    Interface changes

--- Rain Society/Rain Society/RSButton.h
+++ Rain Society/Rain Society/RSButton.h
@@ -1,6 +1,6 @@
 //
 //  RSButton.h
-//  GameZAM
+//  Rain Society
 //
 //  Created by Jucelio Fonseca on 26/03/15.
 //  Copyright (c) 2015 Jucelio Fonseca. All rights reserved.
--- Rain Society/Rain Society/RSStepper.h
+++ Rain Society/Rain Society/RSStepper.h
@@ -1,6 +1,6 @@
 //
 //  RSStepper.h
-//  GameZAM
+//  Rain Society
 //
 //  Created by Jucelio Fonseca on 26/03/15.
 //  Copyright (c) 2015 Jucelio Fonseca. All rights reserved.
