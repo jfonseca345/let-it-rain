e8c3d54557339afdf18ccf500c23081306b5495d
Leticiasg <leticiagoncalves2005@gmail.com>
2015-03-31 11:52:15 -0300

    mais limpeza

--- Rain Society/Rain Society/GameScene.m
+++ Rain Society/Rain Society/GameScene.m
@@ -26,26 +26,6 @@
 
 @implementation GameScene
 
-
-
-- (SKSpriteNode *)Campo
-{
-    SKSpriteNode *hull = [[SKSpriteNode alloc] initWithColor:[SKColor whiteColor] size:CGSizeMake(64,32)];
-    
-    
-    SKAction *hover = [SKAction sequence:@[
-                                           [SKAction waitForDuration:1.0],
-                                           [SKAction moveByX:100 y:50.0 duration:1.0],
-                                           [SKAction waitForDuration:1.0],
-                                           [SKAction moveByX:-100.0 y:-50 duration:1.0]]];
-    [hull runAction: [SKAction repeatActionForever:hover]];
-    
-    return hull;
-
-}
-
-
-
 -(void)didMoveToView:(SKView *)view {
     /* Setup your scene here */
     SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Verdana"];
